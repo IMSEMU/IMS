1f1bde610831b35e0b21cfc41e70b20af339e41d
ahmedgh77 <19700430@emu.edu.tr>
2023-10-30 22:32:08 +0200

    report page

--- client/src/app/[locale]/appForm/components/formPage.jsx
+++ client/src/app/[locale]/appForm/components/formPage.jsx
@@ -1,43 +1,43 @@
 "use client";
 
-import { AppForm } from "./appForm";
-import AuthConnect from "@/auth";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { TopNav } from "../../internDashboard/components/topNav";
-import { PcSideNav } from "../../globalComponents/pcSideNav";
-import { MobileNav } from "../../globalComponents/mobileNav";
-import { ProtectedRoute } from "../../globalComponents/stdProtectedRoute";
-import jwtDecode from "jwt-decode";
+ import { AppForm } from "./appForm";
+// import AuthConnect from "@/auth";
+// import { useRouter } from "next/navigation";
+// import { useEffect, useState } from "react";
+ import { TopNav } from "../../internDashboard/components/topNav";
+ import { PcSideNav } from "../../globalComponents/pcSideNav";
+ import { MobileNav } from "../../globalComponents/mobileNav";
+// import { ProtectedRoute } from "../../globalComponents/stdProtectedRoute";
+// import jwtDecode from "jwt-decode";
 
-export const ApplicationForm = () => {
-  const router = useRouter();
-  const [user, setUser] = useState(null);
+ export const ApplicationForm = () => {
+//   const router = useRouter();
+//   const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const getToken = async () => {
-      try {
-        const response = await AuthConnect.get("/token");
+//   useEffect(() => {
+//     const getToken = async () => {
+//       try {
+//         const response = await AuthConnect.get("/token");
 
-        // Decode the token immediately after setting it
-        try {
-          const decoded = jwtDecode(response.data);
-          setUser(decoded);
-        } catch (error) {
-          console.error("Error decoding token:", error);
-        }
-      } catch (error) {
-        console.error("User not authenticated", error);
-        router.push("/login");
-      }
-    };
+//         // Decode the token immediately after setting it
+//         try {
+//           const decoded = jwtDecode(response.data);
+//           setUser(decoded);
+//         } catch (error) {
+//           console.error("Error decoding token:", error);
+//         }
+//       } catch (error) {
+//         console.error("User not authenticated", error);
+//         router.push("/login");
+//       }
+//     };
 
-    getToken();
-  }, []);
+//     getToken();
+//   }, []);
 
-  if (!user) {
-    return null; // Prevent rendering the dashboard until token is fetched
-  }
+//   if (!user) {
+//     return null; // Prevent rendering the dashboard until token is fetched
+//   }
 
   return (
     <main className="bg-white dark:bg-dark_1 h-screen">
@@ -45,9 +45,9 @@ export const ApplicationForm = () => {
         <PcSideNav />
         <div className={"h-full w-full"}>
           <TopNav />
-          <ProtectedRoute>
+          
             <AppForm />
-          </ProtectedRoute>
+          
         </div>
 
         <MobileNav />
