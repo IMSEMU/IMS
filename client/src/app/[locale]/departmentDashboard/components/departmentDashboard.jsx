199ca79a9ec466a93795c00c2f44459602bf386c
Lateefah <teefahlateef@outlook.com>
2023-12-09 19:30:07 +0200

    add: authentication for dept and comp dash

--- client/src/app/[locale]/departmentDashboard/components/departmentDashboard.jsx
+++ client/src/app/[locale]/departmentDashboard/components/departmentDashboard.jsx
@@ -4,8 +4,37 @@ import { MobileNav } from "../../globalComponents/mobileNav";
 import { PcSideNav } from "../../globalComponents/pcSideNav";
 import { Dashboard } from "./dashboard";
 import { TopNav } from "../../globalComponents/topNav";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export const DepartmentDashboardPage = () => {
+  const router = useRouter();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getToken = async () => {
+      try {
+        const response = await AuthConnect.get("/token");
+
+        // Decode the token immediately after setting it
+        try {
+          const decoded = jwtDecode(response.data);
+          setUser(decoded);
+        } catch (error) {
+          console.error("Error decoding token:", error);
+        }
+      } catch (error) {
+        console.error("User not authenticated", error);
+        router.push("/login");
+      }
+    };
+
+    getToken();
+  }, []);
+
+  if (!user) {
+    return null; // Prevent rendering the dashboard until token is fetched
+  }
   return (
     <main className={"p-0 m-0 bg-white dark:bg-dark_2"}>
       {/*Sidenav and body*/}
