fe22e73611e3209f57c31036148d848ee266be74
tahirarim21 <118083230+tahirarim21@users.noreply.github.com>
2023-12-19 17:15:34 +0200

    Update adminDashboard.jsx
    comment

--- client/src/app/[locale]/adminDashboard/components/adminDashboard.jsx
+++ client/src/app/[locale]/adminDashboard/components/adminDashboard.jsx
@@ -1,18 +1,21 @@
 "use client";
+// Importing necessary components and libraries
 import { MobileNav } from "../../globalComponents/mobileNav";
 import { PcSideNav } from "../../globalComponents/pcSideNav";
 import { Dashboard } from "./dashboard";
 import { TopNav } from "../../globalComponents/topNav";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import AuthConnect from "@/auth";
+import AuthConnect from "@/auth";  // Assuming this is a custom authentication module
 import jwtDecode from "jwt-decode";
 import { ProtectedRoute } from "../../globalComponents/adminProtectedRoute";
 
+// Functional component for the Admin Dashboard page
 export const AdminDashboardPage = () => {
   const router = useRouter();
   const [user, setUser] = useState(null);
 
+  // Fetching and decoding the user token on component mount
   useEffect(() => {
     const getToken = async () => {
       try {
@@ -27,28 +30,38 @@ export const AdminDashboardPage = () => {
         }
       } catch (error) {
         console.error("User not authenticated", error);
-        router.push("/login");
+        router.push("/login");  // Redirecting to login page if authentication fails
       }
     };
 
     getToken();
   }, []);
 
+  // If user is not available, prevent rendering the dashboard until token is fetched
   if (!user) {
-    return null; // Prevent rendering the dashboard until token is fetched
+    return null;
   }
+
+  // Render the main content of the Admin Dashboard page
   return (
     <main className={"p-0 m-0 bg-white dark:bg-dark_2"}>
-      {/*Sidenav and body*/}
+      {/* Sidenav and body */}
       <div className={"flex flex-nowrap"}>
+        {/* PC Side Navigation with user information */}
         <PcSideNav user={user} />
+
+        {/* Main content area */}
         <div className={"h-full w-full"}>
+          {/* Top navigation bar with user information */}
           <TopNav user={user} />
+
+          {/* Protected route for rendering the Dashboard component */}
           <ProtectedRoute>
             <Dashboard user={user} />
           </ProtectedRoute>
         </div>
 
+        {/* Mobile navigation component */}
         <MobileNav />
       </div>
     </main>
